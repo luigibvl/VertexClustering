1f7b969f907d2d380568d7b740fd76f2f1449dcc
Ebtehal El marzouki <elmarzoukiebtehal@gmail.com>
2019-04-02 18:10:16 +0200

    shingle

--- vertex/src/main/java/agiw/agiw2019/Shingle.java
+++ vertex/src/main/java/agiw/agiw2019/Shingle.java
@@ -1,7 +1,48 @@
 package agiw.agiw2019;
 
+import java.util.List;
+
 public class Shingle {
 	
-	String tag_sequence;
+	List<String> tag_sequence;
+	int length;
+	
+	public Shingle(int length) {
+		this.length=length;
+	}
+	
+	
+	
+	public List<String> getTag_sequence() {
+		return tag_sequence;
+	}
+
+
+
+	public void setTag_sequence(List<String> tag_sequence) {
+		this.tag_sequence = tag_sequence;
+	}
+
+
+	public String concatTagSequence() {
+		String stringTag="";
+		for(String tag : tag_sequence) {
+			stringTag= stringTag.concat(tag);
+		}
+		return stringTag;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+
+
 
 }
