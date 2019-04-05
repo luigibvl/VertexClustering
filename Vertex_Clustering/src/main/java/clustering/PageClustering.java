c371555342a0ba9a99748a353bae6a75ce18112b
Eleonora <Eleonora@Hikari777Yami>
2019-04-05 13:38:14 +0200

    second pass

--- Vertex_Clustering/src/main/java/clustering/PageClustering.java
+++ Vertex_Clustering/src/main/java/clustering/PageClustering.java
@@ -13,7 +13,7 @@ public class PageClustering {
 
 	public PageClustering() {}
 
-	public void algorithm(/*List<TagPagina> sample_pages*/ TagPagina p){
+	public void algorithm(/*List<TagPagina> sample_pages*/TagPagina p){
 		
 		/*First pass*/
 		HashTable table = new HashTable();
@@ -30,7 +30,6 @@ public class PageClustering {
 			
 			System.out.println(v.getMasked_vectors().toString());
 			
-			
 			for (MaskedShingleVector mv : v.getAllMV()){
 				if (table.contains(mv))
 					mv.incrementCount();
@@ -40,10 +39,17 @@ public class PageClustering {
 				}
 			}
 			
-			System.out.println(v.getAllMV().toString());
-		}
+			System.out.println(v.getAllMV().toString()); //OK
+		//}
 		
 		/*Second pass*/
 		
-	//}
+		table.order_by_count();
+		
+		System.out.println(table.toString()); //OK
+		
+		List<MaskedShingleVector> all88MV = table.getAll88MaskedVector();
+		
+		System.out.println(all88MV.toString());
+	}
 }
